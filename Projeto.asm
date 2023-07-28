33f51335f126ca58670b3dcdf956c5b231d9c8d6
multi-megaman <evertonsilva2345meia78@gmail.com>
2023-07-28 13:04:23 -0300

    [Update] cardapio_rm adicionado
    a função para remover um item do cardápio foi implementada, ela já se encontra funcional. Ela identifica o item no cardápio e o remove (se ele existir) e, desloca os bytes a direita do item removido para a esquerda, cobrindo o buraco de memória que ficaria.
    
    Mesmo a função estando implementada, ainda é necessário considerar o caso onde estamos removendo o ultimo item do cardápio, já que nesse caso, não vamos deslocar byte nenhum.

--- Projeto.asm
+++ Projeto.asm
@@ -8,6 +8,7 @@
 #Espacos totais para os 20 itens do cardápio 64x20 = 1.280
 cardapio: .space 1280					    #bytes -> quantidade em bytes reservados para todos os possiveis 20 itens do cardápio
 #Valores extras para o funcionamento do cardapio
+tamanho_total_cardapio: .word 1280		  # int -> tamanho total do cardápio em bytes
 limite_cardapio: .half 20   		                   #int -> indica qual o limite de itens do cardápio
 ponteiro_cardapio: .half 0 		                   #int -> Sempre vai estar apontando para a proxima posição livre do cardapio. Quando chega em limite_cardapio, indica que a proxima posição livre está fora do espaço reservado.
 tamanho_codigo_item_cardapio: .byte 2          #int -> indica o tamanho em bytes reservados para o código do cardápio
@@ -20,11 +21,16 @@ falha_criacao_item_cardapio_codigo_cadastrado: .asciiz "Falha: numero de item ja
 falha_criacao_item_cardapio_codigo_invalido: .asciiz "Falha: codigo de item invalido\n"
 falha_criacao_item_cardapio_cheio: .asciiz "Falha: cardapio cheio\n"
 
+falha_remover_item_cardapio_codigo_nao_cadastrado: .asciiz "Falha: codigo informado nao possui item cadastrado no cardapio\n"
+falha_remover_item_cardapio_codigo_invalido: .asciiz "Falha: codigo de item invalido\n"
+
+#Macros
 .macro print_string(%string)
 	addi $v0, $0, 4
 	la $a0, %string
 	syscall
 .end_macro
+
 #testes
 string_usuario: .space 60
 char: .ascii "a"
@@ -43,94 +49,103 @@ add $a2, $0, $a0 #armazena o valor lido em $a2 (string do usuario)
 #---Valores de teste referentes ao id do item ($a0) e ao preço dele ($a1)
 addi $a0, $0, 1
 addi $a1, $0, 1000
-jal cardapio_add
-jal cardapio_add #Erro: Item já cadastrado
-jal cardapio_add #Erro: Item já cadastrado
+jal cardapio_ad
+jal cardapio_ad #Erro: Item já cadastrado
+jal cardapio_ad #Erro: Item já cadastrado
 
 addi $a0, $0, 2
 addi $a1, $0, 2000
-jal cardapio_add
+jal cardapio_ad
 
 addi $a0, $0, 3
 addi $a1, $0, 3000
-jal cardapio_add
+jal cardapio_ad
 
 addi $a0, $0, 4
 addi $a1, $0, 70
-jal cardapio_add
+jal cardapio_ad
 
 addi $a0, $0, 5
 addi $a1, $0, 6467
-jal cardapio_add
+jal cardapio_ad
 
 addi $a0, $0, 10
 addi $a1, $0, 99999
-jal cardapio_add
+jal cardapio_ad
 
 addi $a0, $0, 9
 addi $a1, $0, 99999
-jal cardapio_add
+jal cardapio_ad
 
 addi $a0, $0, 7
 addi $a1, $0, 999
-jal cardapio_add
+jal cardapio_ad
 
 addi $a0, $0, 8
 addi $a1, $0, 99999
-jal cardapio_add
+jal cardapio_ad
 
 addi $a0, $0, 6
 addi $a1, $0, 6893
-jal cardapio_add
+jal cardapio_ad
 
 addi $a0, $0, 11
 addi $a1, $0, 67812
-jal cardapio_add
+jal cardapio_ad
 
 addi $a0, $0, 15
 addi $a1, $0, 28
-jal cardapio_add
+jal cardapio_ad
 
 addi $a0, $0, 12
 addi $a1, $0, 1500
-jal cardapio_add
+jal cardapio_ad
 
 addi $a0, $0, 13
 addi $a1, $0, 67278
-jal cardapio_add
+jal cardapio_ad
 
 addi $a0, $0, 14
 addi $a1, $0, 67176
-jal cardapio_add
+jal cardapio_ad
 
 addi $a0, $0, 16
 addi $a1, $0, 16512
-jal cardapio_add
+jal cardapio_ad
 
 addi $a0, $0, 17
 addi $a1, $0, 89321
-jal cardapio_add
+jal cardapio_ad
 
 addi $a0, $0, 18
 addi $a1, $0, 87234
-jal cardapio_add
+jal cardapio_ad
 
 addi $a0, $0, 19
 addi $a1, $0, 17821
-jal cardapio_add
+jal cardapio_ad
 
-addi $a0, $0, 20
-addi $a1, $0, 78214
-jal cardapio_add
+#addi $a0, $0, 20
+#addi $a1, $0, 78214
+#jal cardapio_ad
 
 addi $a0, $0, 26
 addi $a1, $0, 78214
-jal cardapio_add # Erro: Código fora do alcance
+jal cardapio_ad # Erro: Código inválido
 
 addi $a0, $0, 0
 addi $a1, $0, 78214
-jal cardapio_add # Erro: Código fora do alcance
+jal cardapio_ad # Erro: Código inválido
 
+#Remover item
+addi $a0, $0, 21
+jal cardapio_rm #Erro: código inválido
+
+addi $a0, $0, 20
+jal cardapio_rm #Erro: código não cadastrado
+
+addi $a0, $0, 4
+jal cardapio_rm #Sucesso
 
 #Checando a existencia de um código no cardápio
 addi $a0, $0, 3
@@ -141,9 +156,9 @@ addi $v0, $0, 10
 syscall
 
 #=====Criar item no cardápio=====
-cardapio_add: #Params ($a0 -> codigo do item  | int        1 byte,
+cardapio_ad: #Params ($a0 -> codigo do item  | int          2 bytes,
 				     #$a1 ->  preco do item   | int        2 bytes,
-				     #$a2 -> descricao	    | string 64 bytes)
+				     #$a2 -> descricao	    | string 60 bytes)
 	#Registradores temporarios utilizados:
 	#$t0 -> limite_cardapio
 	#$t1 -> endereço do ponteiro_cardapio
@@ -214,7 +229,7 @@ cardapio_add: #Params ($a0 -> codigo do item  | int        1 byte,
 		addi $sp, $sp, 4 #voltando a pilha pro lugar original
 		
 		addi $v0, $0, 1 #1 significa falha
-		j fim_cardapio_add
+		j fim_cardapio_ad
 	falha_cardapio_codigo_existe:
 		addi $sp, $sp, -4
 		sw $a0, 0($sp) #Salvando o valor de $a0 para poder voltar a funcao
@@ -222,7 +237,7 @@ cardapio_add: #Params ($a0 -> codigo do item  | int        1 byte,
 		lw $a0, 0($sp)	#Recupenrando o $a0 antigo
 		addi $sp, $sp, 4 #voltando a pilha pro lugar original
 		addi $v0, $0, 1
-		j fim_cardapio_add
+		j fim_cardapio_ad
 	falha_cardapio_codigo_alcance:
 		addi $sp, $sp, -4
 		sw $a0, 0($sp) #Salvando o valor de $a0 para poder voltar a funcao
@@ -230,11 +245,81 @@ cardapio_add: #Params ($a0 -> codigo do item  | int        1 byte,
 		lw $a0, 0($sp)	#Recupenrando o $a0 antigo
 		addi $sp, $sp, 4 #voltando a pilha pro lugar original
 		addi $v0, $0, 1
-		j fim_cardapio_add
+		j fim_cardapio_ad
 	sucesso:
 		addi $v0, $0, 0
-	fim_cardapio_add:
-jr $ra		#Return ($v0 -> sucesso(0)/fracasso(1) | bool
+	fim_cardapio_ad:
+jr $ra		#Return ($v0 -> sucesso(0)/fracasso(1) | bool)
+
+#===== Deletar item do cardápio =====
+cardapio_rm:  #Params ($a0 -> codigo do item  | int  2 byts)
+
+	#== Checando para ver se o código digitado está entre 1-20
+	bge $a0, 1, rm_checar_limite_superior # Se $a0 >= 1
+	j falha_cardapio_rm_codigo_alcance  # Pula para o final da função
+	rm_checar_limite_superior:
+	ble $a0, $t0, rm_dentro_do_limite    # Se $a0 <= limite_cardapio(20) significa que ele está dentro do limite 1-20
+	j falha_cardapio_codigo_alcance  # Pula para o final da função
+	rm_dentro_do_limite:
+	
+	#== Checando para ver se o código existe dentro do cardápio
+	addi $sp, $sp, -4
+	sw $ra, 0($sp) #Salvando o valor de $ra para poder voltar a funcao
+	jal checar_existencia_de_codigo #verificar se o código inserido realmente existe no cardápio
+	lw $ra, 0($sp)	#Recupenrando o $ra  antigo
+	addi $sp, $sp, 4 #voltando a pilha pro lugar original
+	beqz $v0, falha_cardapio_rm_codigo_inexistente #Se o retorno de checar_existencia_de_codigo for 0, significa que o código inserido não existe
+	
+	#== Caso o código seja válido e ele exista, vamos "Deletar a entrada" movendo todos os bytes a direita desse item do cardápio para a esquerda
+	addi $sp, $sp, -16
+	sw $ra, 0($sp) #Salvando o valor de $ra para poder voltar a funcao
+	sw $a0, 4($sp)#Salvando o valor de $a0
+	sw $a1, 8 ($sp)#Salvando o valor de $a1
+	sw $a2, 12($sp)#Salvando o valor de $a2
+	#=========
+	la $t4, cardapio
+	lbu $t0, tamanho_total_item_cardapio #Carregando o tamanho total de um item no cardapio
+	multu $v1, $t0 # Calculando o offset necessário para ir diretamente para a primeira posição do item que será "deletado"
+	mflo $t1          #offset para se chegar no item que vai ser "deletado"
+	add $a2, $t4, $t1 #a2 está apontando exatamente para o local inicial onde o conjunto vai ser copiado (apontando para o item que vai ser "deletado")
+	add $a0, $a2, $t0 # $a0 será o proximo item referente ao item que vai ser "deletado"
+	lw $t2, tamanho_total_cardapio 
+	add $a1, $t2, $t4 #Calculando o endereço final do cardápio
+	jal shift_mem_left #Params ($a0 -> local inicial do conjunto de bytes que vai ser copiado | memory address,
+					 # $a1 -> local final do conjunto de bytes que vai ser copiado   | memory address,
+					 # $a2 -> local inicial para onde o conjunto vai ser copiado       | memory address)
+	lw $a2, 12($sp)#Carregando de volta $a2 
+	lw $a1, 8($sp)#Carregando de volta $a1
+	lw $a0, 4($sp)#Carregando de volta $a0
+	lw $ra, 0($sp)#Carregando de volta $a2 
+	addi $sp, $sp, 16 
+	
+	lhu $t2,  ponteiro_cardapio
+	la $t1, ponteiro_cardapio
+	subiu $t2, $t2, 1	#Subtrai 1 ao ponteiro_cardapio
+	sh $t2, 0($t1) 	#armazena o valor ponteiro_cardapio - 1 no endereço do ponteiro_cardapio
+	
+j fim_cardapio_rm
+falha_cardapio_rm_codigo_inexistente:
+		addi $sp, $sp, -4
+		sw $a0, 0($sp) #Salvando o valor de $a0 para poder voltar a funcao
+		print_string(falha_remover_item_cardapio_codigo_nao_cadastrado)
+		lw $a0, 0($sp)	#Recupenrando o $a0 antigo
+		addi $sp, $sp, 4 #voltando a pilha pro lugar original
+		
+		addi $v0, $0, 1 #1 significa falha
+		j fim_cardapio_rm
+falha_cardapio_rm_codigo_alcance:
+		addi $sp, $sp, -4
+		sw $a0, 0($sp) #Salvando o valor de $a0 para poder voltar a funcao
+		print_string(falha_remover_item_cardapio_codigo_invalido)
+		lw $a0, 0($sp)	#Recupenrando o $a0 antigo
+		addi $sp, $sp, 4 #voltando a pilha pro lugar original
+
+		addi $v0, $0, 1 #1 significa falha
+		j fim_cardapio_rm
+fim_cardapio_rm: 
+jr $ra	       #Return ($v0 -> sucesso (0)/fracasso(1) | bool)
 
 
 #===== Chegar se um id especifico já está no cardapio ===== 
@@ -268,6 +353,30 @@ checar_existencia_de_codigo: #Params($a0 -> codigo do item | int)
 	fim_checar_existencia_de_codigo:
 jr $ra # Return($v0 -> 1 se o código já existe e 0 caso ele não exista | bool
 			#$v1 -> Posição no cardápio onde esse item foi encontrado | int)
+
+
+#===== Deslocar uma quantidade x ($a1 - $a0) de bits para a esquerda ($a2)
+shift_mem_left: #Params ($a0 -> local inicial do conjunto de bytes que vai ser copiado | memory address,
+					 # $a1 -> local final do conjunto de bytes que vai ser copiado   | memory address,
+					 # $a2 -> local inicial para onde o conjunto vai ser copiado       | memory address)
+#Registradores temporarios utilizados:
+#$t0 -> vai percorrer o conjunto de bits e vai copia-los
+#$t1 -> vai percorrer, junto com o $t0 e vai servir para $t0 copiar o byte para $t1
+#$t2 -> byte que estará na posição $t0
+add  $t0, $0, $a0 # começa no valor inicial de copia
+add $t1, $0, $a2 # começa nop valor inicial do destino
+lb $t2, 0($t0)
+#Adicionar um caso onde $a1 (fim do conjunto de bytes que vai ser copiado) é igual a $a2 (para onde os bytes vão ser copiados) isso se encaixa no caso onde queremos remover o ultimo item da lista, logo, esse código não funcionaria atualmente sem isso
+loop_shift_mem_left:
+	beq $t0, $a1, fim_shift_mem_left
+	sb $t2, 0($t1) #copiando o byte para o destino a esquerda
+	sb $0, 0($t0) #apagando o conteudo anterior, já que ele já foi copiado
+	addi $t0, $t0, 1 #indo para o próximo endereço de memória
+	addi $t1, $t1, 1 #indo para o próximo endereço de memória
+	lb $t2, 0($t0) #carregando o proximo byte
+	j loop_shift_mem_left
+fim_shift_mem_left:
+jr $ra		   #Return (None)
 
 #===== Copiar String ===== 
 strcpy:	#função que copia uma string
